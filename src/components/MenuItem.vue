35e3ccd82bb25653636b4e9177530a8e5141469c
João Dal Lago <joaogabrieldallago@hotmail.com>
2024-07-04 21:33:46 -0300

    :recycle: Refactor: Estilo menu

--- src/components/MenuItem.vue
+++ src/components/MenuItem.vue
@@ -1,15 +1,19 @@
 <template>
-    <v-btn :to="to" text>
-      {{ text }}
-    </v-btn>
-  </template>
-  
-  <script setup lang="ts">
-  import { defineProps } from 'vue';
-  
-  const props = defineProps<{
-    to: string;
-    text: string;
-  }>();
-  </script>
-  
+  <v-btn 
+    :to="to"
+    variant="plain"
+    color="brown-darken-1"
+    class="cursor-pointer"
+    >
+    {{ text }}
+  </v-btn>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+const props = defineProps<{
+  to: string;
+  text: string;
+}>();
+</script>
