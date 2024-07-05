b46ce507e2f7606e9de38a2112f491952aca41a3
João Dal Lago <joaogabrieldallago@hotmail.com>
2024-07-04 21:40:07 -0300

    :bug: Fix: Correção identação

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,43 +1,41 @@
 <template>
-<v-app-bar color="green-lighten-2" density="prominent">
+  <v-app-bar color="green-lighten-2" density="prominent">
     <v-container class="d-flex justify-center align-center">
-        <div class="text-center flex-grow-1">
-            <MenuItem text="Experiências" to="experience" />
-            <MenuItem text="Projetos" to="project" />
-            <MenuItem text="Imagens" to="image" />
-            <MenuItem text="Contato" to="contact" />
-        </div>
+      <div class="text-center flex-grow-1">
+        <MenuItem text="Experiências" to="experience" />
+        <MenuItem text="Projetos" to="project" />
+        <MenuItem text="Imagens" to="image" />
+        <MenuItem text="Contato" to="contact" />
+      </div>
 
-        <v-icon @click="toggleTheme" class="theme-icon ml-auto">
-            mdi-lightbulb
-        </v-icon>
+      <v-icon @click="toggleTheme" class="theme-icon ml-auto">
+        mdi-lightbulb
+      </v-icon>
     </v-container>
-</v-app-bar>
+  </v-app-bar>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 import MenuItem from './MenuItem.vue';
-import {
-    useTheme
-} from 'vuetify';
+import { useTheme } from 'vuetify';
 
 const theme = useTheme();
 
 function toggleTheme() {
-    theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark';
+  theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark';
 }
 </script>
 
 <style scoped>
 .theme-icon {
-    opacity: 0.6;
-    transition: opacity 0.3s ease;
-    color: whitesmoke;
-    font-size: 32px;
+  opacity: 0.6;
+  transition: opacity 0.3s ease;
+  color: whitesmoke;
+  font-size: 32px;
 }
 
 .theme-icon:hover {
-    opacity: 1;
-    text-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+  opacity: 1;
+  text-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
 }
 </style>
