a88ef8b86d3b048f387a196c5a22a58568475f91
João Dal Lago <joaogabrieldallago@hotmail.com>
2024-08-04 20:10:43 -0300

    :sparckles: Feat: Componente para banner principal

--- architecture-portfolio/src/App.vue
+++ architecture-portfolio/src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import NavBar from "@/components/Organisms/NavBar.vue";
+import MainBanner from "@/components/Organisms/MainBanner.vue";
 </script>
 
 <template>
   <NavBar />
+  <MainBanner />
   <RouterView />
 </template>
 
