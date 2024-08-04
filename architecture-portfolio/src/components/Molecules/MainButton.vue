4dac3e7b8861d5db70082c7f3b1aa2d55390d775
João Dal Lago <joaogabrieldallago@hotmail.com>
2024-08-04 20:59:57 -0300

    :sparckles: Feat: Componente para botão principal

---
+++ architecture-portfolio/src/components/Molecules/MainButton.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+  import { defineProps } from 'vue';
+
+  interface Props {
+    text: string;
+    url: string;
+  }
+  const props = defineProps<Props>();
+</script>
+
+<template>
+  <button href="{{ url }}" class="bg-contrastGold text-primaryText rounded-md p-2">
+    {{ props.text }}
+  </button>
+</template>
+
+<style scoped>
+
+</style>
--- architecture-portfolio/src/components/Organisms/MainBanner.vue
+++ architecture-portfolio/src/components/Organisms/MainBanner.vue
@@ -1,21 +1,25 @@
 <script setup lang="ts">
+import MainButton from "@/components/Molecules/MainButton.vue";
 </script>
 
 <template>
   <div class="w-11/12 p-4 mt-10 mx-auto bg-background py-4 rounded-md relative">
     <div class="flex items-start space-x-4">
-
       <img src="../../assets/temp-images/iara-main-banner.png" class="w-72 rounded-md" alt="Banner"/>
 
-      <p class="flex-1 text-primaryText text-base">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eu quam est. Morbi id lorem turpis. Sed mattis nisl id arcu iaculis, eget eleifend nulla dictum. Phasellus sed tempor metus. Phasellus sollicitudin risus ligula, a volutpat nisl fermentum nec. Etiam sollicitudin lectus in mi vehicula pharetra. Morbi suscipit ultricies felis, sit amet vehicula mi molestie et. Nunc eu viverra neque, eget blandit ante. Vestibulum aliquet quam purus, ut tincidunt est sagittis sed.
-        Donec quis risus ac dolor lobortis ornare. Etiam quis enim sed mi iaculis vehicula non ac leo. Aenean dignissim auctor convallis. Pellentesque bibendum nec orci ac consequat. Nullam vitae tortor enim. Quisque luctus venenatis leo, vitae placerat diam accumsan a. Aenean at nunc a lectus imperdiet placerat. Fusce tincidunt sodales tristique.
-      </p>
+      <div class="flex flex-col justify-between flex-1">
+        <p class="text-primaryText text-base">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eu quam est. Morbi id lorem turpis. Sed mattis nisl id arcu iaculis, eget eleifend nulla dictum. Phasellus sed tempor metus. Phasellus sollicitudin risus ligula, a volutpat nisl fermentum nec. Etiam sollicitudin lectus in mi vehicula pharetra. Morbi suscipit ultricies felis, sit amet vehicula mi molestie et. Nunc eu viverra neque, eget blandit ante. Vestibulum aliquet quam purus, ut tincidunt est sagittis sed. Donec quis risus ac dolor lobortis ornare. Etiam quis enim sed mi iaculis vehicula non ac leo. Aenean dignissim auctor convallis. Pellentesque bibendum nec orci ac consequat. Nullam vitae tortor enim. Quisque luctus venenatis leo, vitae placerat diam accumsan a. Aenean at nunc a lectus imperdiet placerat. Fusce tincidunt sodales tristique.
+        </p>
 
+        <div class="flex justify-center mt-4">
+          <MainButton text="Ver currículo" url="#" class="self-end"/>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <style scoped>
-
+/* Estilize conforme necessário */
 </style>
