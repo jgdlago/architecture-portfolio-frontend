066da70c7b3354272aa98a06da7d1ffc5a39e9e5
João Dal Lago <joaogabrieldallago@hotmail.com>
2024-08-04 19:25:19 -0300

    :sparckles: Feat: Troca de tema na NavBar.vue

--- architecture-portfolio/src/App.vue
+++ architecture-portfolio/src/App.vue
@@ -1,16 +1,9 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import NavBar from "@/components/Organisms/NavBar.vue";
-import { useDark } from '@vueuse/core'
-
-const isDark = useDark()
 </script>
 
 <template>
-  <div class="grid place-content-center">
-    <input type="checkbox" v-model="isDark">
-  </div>
-
   <NavBar />
   <RouterView />
 </template>
--- architecture-portfolio/src/components/Organisms/NavBar.vue
+++ architecture-portfolio/src/components/Organisms/NavBar.vue
@@ -1,14 +1,23 @@
 <script setup lang="ts">
 import MenuItem from "@/components/Atoms/MenuItem.vue";
+import { useDark } from "@vueuse/core";
+
+const isDark = useDark();
 </script>
 
 <template>
-  <div class="w-11/12 mx-auto bg-background py-4 rounded-md">
-    <div class="flex justify-center items-center">
-      <MenuItem text="Experiências" url="#"/>
-      <MenuItem text="Projetos" url="#"/>
-      <MenuItem text="Galeria" url="#"/>
-      <MenuItem text="Contato" url="#"/>
+  <div class="w-11/12 mx-auto bg-background py-4 rounded-md relative">
+
+    <div class="flex justify-center items-center space-x-4">
+      <MenuItem text="Experiências" url="#" />
+      <MenuItem text="Projetos" url="#" />
+      <MenuItem text="Galeria" url="#" />
+      <MenuItem text="Contato" url="#" />
+    </div>
+
+
+    <div class="absolute top-4 right-4">
+      <input type="checkbox" v-model="isDark" />
     </div>
   </div>
 </template>
