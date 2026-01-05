609569749fc2ff5fa532268182e101fc3cbf37e3
João Dal Lago <joaogabrieldallago@hotmail.com>
2026-01-05 03:21:03 -0300

    feat: Adiciona a tela de login com integração à store de autenticação

---
+++ src/views/auth/LoginView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { useAuthStore } from '@/stores/auth';
+
+const auth = useAuthStore()
+</script>
+
+<template>
+  <div>
+    <h1>Login</h1>
+    <button @click="auth.login({ email: 'test@test.com', password: 'password' })">
+      Login
+    </button>
+  </div>
+</template>
